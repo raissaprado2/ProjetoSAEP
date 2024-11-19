97a11caf29696b8079d514e1b83ad0037277132b
raissaprado2 <ra.pradoviturino@gmail.com>
2024-11-19 14:38:31 -0300

    19/11

--- myproject/app.py
+++ myproject/app.py
@@ -1,21 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, Usuario, Tarefa
+import os
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
-db.init_app(app)
 
-migrate = Migrate(app, db)  
+# Configurações do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/nomedobanco'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24)
 
-# Rota para a página inicial
-@app.route('/') 
+# Inicializando o banco de dados e migrações
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Modelos de banco de dados
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    tarefas = db.relationship('Tarefa', backref='usuario', lazy=True)
+
+class Tarefa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(255), nullable=False)
+    setor = db.Column(db.String(100), nullable=False)
+    prioridade = db.Column(db.String(10), nullable=False)
+    status = db.Column(db.String(20), default='a fazer', nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+
+# Rotas
+
+@app.route('/')
 def home():
-    return redirect(url_for('cadastro_usuario'))  # Redireciona para a página de cadastro de usuários
+    return redirect(url_for('cadastro_usuario'))
 
-
-# Rota de cadastro de usuário
 @app.route('/cadastro_usuario', methods=['GET', 'POST'])
 def cadastro_usuario():
     if request.method == 'POST':
@@ -28,7 +48,6 @@ def cadastro_usuario():
         return redirect(url_for('cadastro_usuario'))
     return render_template('cadastro_usuario.html')
 
-# Rota de cadastro de tarefa
 @app.route('/cadastro_tarefa', methods=['GET', 'POST'])
 def cadastro_tarefa():
     usuarios = Usuario.query.all()  # Recuperando usuários cadastrados
@@ -45,42 +64,17 @@ def cadastro_tarefa():
         return redirect(url_for('gerenciar_tarefas'))
     return render_template('cadastro_tarefa.html', usuarios=usuarios)
 
-# Rota para gerenciar tarefas
 @app.route('/gerenciar_tarefas', methods=['GET', 'POST'])
 def gerenciar_tarefas():
     tarefas_a_fazer = Tarefa.query.filter_by(status='a fazer').all()
     tarefas_fazendo = Tarefa.query.filter_by(status='fazendo').all()
     tarefas_pronto = Tarefa.query.filter_by(status='pronto').all()
     
-    return render_template('gerenciar_tarefas.html', tarefas_a_fazer=tarefas_a_fazer, 
-                           tarefas_fazendo=tarefas_fazendo, tarefas_pronto=tarefas_pronto)
+    return render_template('gerenciar_tarefas.html', 
+                           tarefas_a_fazer=tarefas_a_fazer, 
+                           tarefas_fazendo=tarefas_fazendo, 
+                           tarefas_pronto=tarefas_pronto)
 
-# Rota para editar tarefa
-@app.route('/editar_tarefa/<int:tarefa_id>', methods=['GET', 'POST'])
-def editar_tarefa(tarefa_id):
-    tarefa = Tarefa.query.get_or_404(tarefa_id)
-    usuarios = Usuario.query.all()
-    if request.method == 'POST':
-        tarefa.descricao = request.form['descricao']
-        tarefa.setor = request.form['setor']
-        tarefa.prioridade = request.form['prioridade']
-        tarefa.usuario_id = request.form['usuario_id']
-        
-        db.session.commit()
-        flash("Tarefa atualizada com sucesso!")
-        return redirect(url_for('gerenciar_tarefas'))
-    return render_template('editar_tarefa.html', tarefa=tarefa, usuarios=usuarios)
-
-# Rota para excluir tarefa
-@app.route('/excluir_tarefa/<int:tarefa_id>', methods=['POST'])
-def excluir_tarefa(tarefa_id):
-    tarefa = Tarefa.query.get_or_404(tarefa_id)
-    db.session.delete(tarefa)
-    db.session.commit()
-    flash("Tarefa excluída com sucesso!")
-    return redirect(url_for('gerenciar_tarefas'))
-
-# Rota para atualizar status da tarefa
 @app.route('/atualizar_status/<int:tarefa_id>', methods=['POST'])
 def atualizar_status(tarefa_id):
     tarefa = Tarefa.query.get_or_404(tarefa_id)
--- myproject/config.py
+++ myproject/config.py
@@ -4,4 +4,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:minhasenha%40123@localhost/mydatabase'
+    SQLALCHEMY_DATABASE_URI = 'saep'
